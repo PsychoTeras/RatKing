d88b6940f348afb2863873aab8a66744ce4726f9
PsychoTeras <psychoteras@mail.ru>
2015-10-28 03:11:36 +0200

    IC

--- RK.Win/Forms/frmMain.cs
+++ RK.Win/Forms/frmMain.cs
@@ -192,7 +192,7 @@ namespace RK.Win.Forms
                 pbLabyrinth.Image.Dispose();
             }
 
-            ushort width = 4000, height = 4000;
+            ushort width = 2000, height = 2000;
             Bitmap bitmap = new Bitmap(width, height, PixelFormat.Format32bppRgb);
             BitmapData data = bitmap.LockBits(new Rectangle(0, 0, width, height),
                 ImageLockMode.ReadWrite, bitmap.PixelFormat);
