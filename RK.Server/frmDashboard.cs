c85eb2c672bce68b71eac3421d9a3463a35897ec
PsychoTeras <psychoteras@mail.ru>
2015-10-29 16:14:35 +0200

    IC

---
+++ RK.Server/frmDashboard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RK.Server
+{
+    public partial class frmDashboard : Form
+    {
+        public frmDashboard()
+        {
+            InitializeComponent();
+        }
+    }
+}
