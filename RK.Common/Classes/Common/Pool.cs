c5aab629f52210121c4e4928d447dac7a9901f7d
PsychoTeras <psychoteras@mail.ru>
2015-11-04 03:03:04 +0200

    IC

--- RK.Common/Classes/Common/Pool.cs
+++ RK.Common/Classes/Common/Pool.cs
@@ -7,16 +7,16 @@ namespace RK.Common.Classes.Common
         private int _maxItemsCount;
         private const float GROW_FACTOR = 1.5f;
 
-        private readonly Queue<T> _pool;
+        private readonly Stack<T> _pool;
 
         public Pool(int capacity, bool fillItemsPool)
         {
-            _pool = new Queue<T>(capacity);
+            _pool = new Stack<T>(capacity);
             if (fillItemsPool)
             {
                 for (int i = 0; i < capacity; i++)
                 {
-                    _pool.Enqueue(new T());
+                    _pool.Push(new T());
                 }
             }
             _maxItemsCount = _pool.Count;
@@ -31,7 +31,7 @@ namespace RK.Common.Classes.Common
         {
             lock (this)
             {
-                return _pool.Dequeue();
+                return _pool.Pop();
             }
         }
 
@@ -40,7 +40,7 @@ namespace RK.Common.Classes.Common
             lock (this)
             {
                 if (_pool.Count == 0) Expand();
-                return _pool.Dequeue();
+                return _pool.Pop();
             }
         }
 
@@ -48,24 +48,24 @@ namespace RK.Common.Classes.Common
         {
             lock (this)
             {
-                _pool.Enqueue(item);
+                _pool.Push(item);
             }
         }
 
         public T PopAsync()
         {
-            return _pool.Dequeue();
+            return _pool.Pop();
         }
 
         public T PopExpandAsync()
         {
             if (_pool.Count == 0) Expand();
-            return _pool.Dequeue();
+            return _pool.Pop();
         }
 
         public void PushAsync(T item)
         {
-            _pool.Enqueue(item);
+            _pool.Push(item);
         }
 
         private void Expand()
@@ -73,7 +73,7 @@ namespace RK.Common.Classes.Common
             int expandOnCount = (int) (_maxItemsCount*GROW_FACTOR) - _maxItemsCount;
             for (int i = 0; i < expandOnCount; i++)
             {
-                _pool.Enqueue(new T());
+                _pool.Push(new T());
             }
             _maxItemsCount += expandOnCount;
         }
